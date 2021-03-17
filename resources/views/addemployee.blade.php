d0f6f0953f42564f454c310907972fed3f5e0e6d
amalsony505 <mca505@rajagiri.edu>
2021-03-17 14:36:26 +0530

    employee

---
+++ resources/views/addemployee.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Employee</title>
+</head>
+<body>
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="/">Add Employee</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " aria-current="page"  href="/searchemployee">Search employee</a>
+  </li>
+  
+</ul>
+<div class="container">
+<div class="row">
+    <div class="col"></div>
+    <div class="col">
+    <table class="table table-borderless">
+    <tr>
+        <td>Employee Name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Employee Code</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Designation</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Salary</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>DOB</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Place</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Pincode</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Phone No:</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><button class="btn btn-success">Submit</button></td>
+    </tr>
+    </table>
+    </div>
+    <div class="col"></div>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+</body>
+</html>
